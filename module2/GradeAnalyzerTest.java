b2d96486d6f09fbf394c506ae3f8e1ccb47a8925
Charles Thibault <charles.thibault@gmail.com>
2026-08-28 11:31:56 -0700

    Add ContactTest to the repo

--- module2/GradeAnalyzerTest.java
+++ module2/GradeAnalyzerTest.java
@@ -1,5 +1,7 @@
-import org.junit.jupiter.api.Test; 
-import static org.junit.jupiter.api.Assertions.*; 
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList; 
 import java.util.Arrays; 
  
